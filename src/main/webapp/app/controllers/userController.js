22438188e5d53099d8affa794fc37f8f1fb8dfb0
Gaston <mazzeo.gn@gmail.com>
2015-09-08 00:05:05 -0300

    Agregar partidos desde la seccion User

--- src/main/webapp/app/controllers/userController.js
+++ src/main/webapp/app/controllers/userController.js
@@ -1,3 +1,23 @@
-var userController = function ($scope) {
-    $scope.message = 'Estoy en la seccion usuarios!';
+var userController = function($scope, $http) {
+    $http.get("/_ah/api/partidosmanager/v1/partido/")
+        .success(function (response) {$scope.partidos = response.items;});
+
+    $scope.delete = function(index){
+        var partido = $scope.partidos[index];
+        console.log(partido);
+        $http.delete("/_ah/api/partidosmanager/v1/partido/", {'id': partido.id})
+            .success(function (response) {$scope.partidos.splice(index, 1);});
+    }
+
+    $scope.add = function(){
+        $http.post("/_ah/api/partidosmanager/v1/partido/",
+            {
+                'deporte': $scope.deporte,
+                'cant_personas': $scope.cantPersonas,
+                'lugar': $scope.lugar
+            }).success(function(response){
+                    $scope.partidos.push(response);
+                });
+    }
 }
+
