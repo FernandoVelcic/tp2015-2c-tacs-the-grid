bcc8f1862cd2ff5a7ad1f7fb6c62deeed6648962
FernandoVelcic <fernando_velcic_77@hotmail.com>
2015-10-11 21:04:42 -0300

    Arreglo vista de partidos de amigos

--- src/main/webapp/assets/js/controllers/friendsPartidosController.js
+++ src/main/webapp/assets/js/controllers/friendsPartidosController.js
@@ -1,74 +1,33 @@
 app.controller('friendsPartidosController', function($scope, $http, $location, FriendPartido, Inscripto) {
     FriendPartido.query(function(data) {
-        $scope.partidos = data.items;
-        alert("Partidos cargados");
-        $scope.partidosConBool = cargarPartidosConBool(data.items);   //TODO LINEA CON PROBLEMAS
-        //alert("Partidos con Bool cargados");
+        $scope.partidos = data.items.map(function(partido){
+            partido.inscripcion = undefined;
+            return partido;
+        });
+
+        Inscripto.query(function(data) {
+            data.items.forEach(function(inscripto){
+                var partido = $scope.partidos.find(function(p){ return p.id ==  inscripto.partido.id});
+                partido.inscripcion = inscripto;
+            });
+        });
     });
-    Inscripto.query(function(data) {
-        alert("Inscripciones cargadas");
-        $scope.inscriptos = data.items;
-    });
 
-    cargarPartidosConBool = function(partidos){
-        alert("Partidos: " + partidos.length + "- Inscriptos: " + $scope.inscriptos.length);
-        var listaTemp = new Array();
-        var i;
-        for (i = 0; i < partidos.length; i++) {
-            listaTemp.push(new PartidoConBool(partidos[i], estoyInscriptoEn(partidos[i])));
-        }
-        return listaTemp;
-    }
-
-    estoyInscriptoEn = function(partido) {
-        var i;
-        for (i = 0; i < $scope.inscriptos.length; i++) {
-            //alert("partido" + i);
-            if ($scope.inscriptos[i].partido.id == partido.id) {
-                return true;
-            }
-        }
-        return false;
-    };
-
-    $scope.anotarme = function(partidoConBool){
+    $scope.anotarme = function(partido){
         var inscripto = {
             'partido': {
-                'id': partidoConBool.partido.id
+                'id': partido.id
             }
         };
 
         Inscripto.save(inscripto, function(response){
-            $scope.inscriptos.push(response);
-            partidoConBool.estoyInscripto = true;
+            partido.inscripcion = response;
         });
     };
 
-    $scope.desanotarme = function(partidoConBool){
-        var inscripto = inscriptoDe(partidoConBool.partido);
-        if(inscripto == null) return;
-
-        alert(inscripto.id);
-
-        Inscripto.delete({ id: inscripto.id}, function (response) {
-                $scope.inscriptos.splice($scope.inscriptos.indexOf(inscripto), 1);
-                partidoConBool.estoyInscripto = false;
-            });
+    $scope.desanotarme = function(partido){
+        Inscripto.delete({ id: partido.inscripcion.id}, function (response) {
+            partido.inscripcion = undefined;
+        });
     };
-
-    function inscriptoDe(partido) {
-        for (var j = 0; j < $scope.inscriptos.length; j++) {
-            if ($scope.inscriptos[j].partido.id == partido.id) {
-                return $scope.inscriptos[j];
-            }
-        }
-        return null;
-    }
-
-    //----------Prototipos-----------
-
-    function PartidoConBool(partido, estoyInscriptoEn) {
-        this.partido = partido;
-        this.estoyInscripto = estoyInscriptoEn;
-    }
 });
