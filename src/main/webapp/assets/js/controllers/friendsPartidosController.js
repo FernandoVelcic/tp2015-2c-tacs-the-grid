5fb10c4de4d751c8ad3fea1857f26c1a6a9083f9
BMendez <mendezbruno.sist.info@gmail.com>
2015-10-15 23:47:21 -0300

    cuenta los inscriptos al partido

--- src/main/webapp/assets/js/controllers/friendsPartidosController.js
+++ src/main/webapp/assets/js/controllers/friendsPartidosController.js
@@ -1,17 +1,29 @@
 app.controller('friendsPartidosController', function($scope, $http, $location, FriendPartido, Inscripto, AccionesPartido) {
     $scope.accionesPartido = AccionesPartido;
 
+
+
     FriendPartido.query(function(data) {
         $scope.partidos = data.items.map(function(partido){
             partido.inscripcion = undefined;
             return partido;
         });
 
+            $scope.partidos.forEach(function(partido){
+                partido.cantInscriptos=0;
+            });
+
+
         Inscripto.query(function(data) {
             data.items.forEach(function(inscripto){
                 var partido = $scope.partidos.find(function(p){ return p.id ==  inscripto.partido.id});
                 partido.inscripcion = inscripto;
+                partido.cantInscriptos++;
             });
+
+
         });
+
+
     });
 });
