de8e99776a5e919213479df764c2ec010b9bf73f
FernandoVelcic <fernando_velcic_77@hotmail.com>
2015-11-02 13:47:54 -0300

    cuenta los inscriptos al partido (reverted from commit 5fb10c4de4d751c8ad3fea1857f26c1a6a9083f9)

--- src/main/webapp/assets/js/controllers/friendsPartidosController.js
+++ src/main/webapp/assets/js/controllers/friendsPartidosController.js
@@ -7,15 +7,10 @@ app.controller('friendsPartidosController', function($scope, $http, $location, F
             return partido;
         });
 
-        $scope.partidos.forEach(function(partido){
-            partido.cantInscriptos=0;
-        });
-
         Inscripto.query(function(data) {
             data.items.forEach(function(inscripto){
                 var partido = $scope.partidos.find(function(p){ return p.id ==  inscripto.partido.id});
                 partido.inscripcion = inscripto;
-                partido.cantInscriptos++;
             });
         });
     });
