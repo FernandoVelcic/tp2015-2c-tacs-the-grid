2edf2c99376bd3944c77f14bf1985ea236269cf2
EzequielAyzenberg <ezequiel_ay@hotmail.com>
2015-11-14 12:17:01 -0300

    Arreglo link de Volver a PartidosManager

--- src/main/webapp/admin/assets/js/controllers/mainController.js
+++ src/main/webapp/admin/assets/js/controllers/mainController.js
@@ -1,3 +1,6 @@
-adminapp.controller('mainController', function($scope, $rootScope, $http) {
-
+adminapp.controller('mainController', function($scope, $location, $window) {
+    $scope.goHome = function() {
+        $location.path('/');
+        $window.location.reload();
+    };
 });
